902d69a0d1202c725e7b62f6436898edbcdad67e
Noh Jihyun <blue900823@gmail.com>
2026-01-23 15:37:59 +0900

    2026.01.23 카테고리페이지 소스분석 정리

--- src/pages/admin/CategoryPage.jsx
+++ src/pages/admin/CategoryPage.jsx
@@ -10,20 +10,25 @@ export default function CategoryPage() {
     const [selected, setSelected] = useState(null);
     const [toast, setToast] = useState({ open:false, msg:'', sev:'success' });
 
+    //비동기
     //LIST
+    //백단에서 return json 배열 , 앞단에서 [] 배열 처리후 세터함수로 화면 렌더
     const load = async () => {
         try {
-            const data = await api.getCategories();               // <-- 배열만 들어옴
-            const list = Array.isArray(data) ? data : (data?.content ?? []);
-            setRows(list);
+            const data = await api.getCategories();                                      //서버에서 응답된 데이터 할당, json 배열만 들어옴
+            const list = Array.isArray(data) ? data : (data?.content ?? []);   //방어코드 어떠한 형태로오든 결국 list => [] 배열로 만듬, content 페이징처리 미리예방
+            setRows(list);                                                              //list에 담긴 데이터를 useState로 상태값 변경
             console.log('categories:', list, list.length);
         } catch {
-            setRows([]);
+            setRows([]);                                                          //화면이 터지지 않게 아닐시 빈배열로 만들어둠
         }
     };
 
+    //useEffect는 첫 렌더링이 끝난 뒤에 (해야 할 일을) 지시하는 훅이다. => 1.서버에서 데이터 요청 2.자동으로 수행해야될 다음작업 
+    //의존성 배열이 빈 배열이면 useEffect 안의 함수는 한 번만 실행되고, 의존성 배열 안에 변수가 있으면 그 변수가 바뀔 때마다 실행된다.
     useEffect(() => { load(); }, []);
-   //INSERT
+
+    //INSERT
     const handleSave = async (form) => {
         try {
             if (selected?.categoryId) await api.updateCategory(selected.categoryId, form);
